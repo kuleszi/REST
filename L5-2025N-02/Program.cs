61b7dd959275361fdc7a820627774d81dfda8121
kuleszi <aleksandrakulesza.96@gmail.com>
2026-06-20 14:21:45 +0200

    UseCor

--- L5-2025N-02/Program.cs
+++ L5-2025N-02/Program.cs
@@ -36,6 +36,11 @@ builder.Services.AddScoped<TokenService>();
 
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy => 
+    policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
@@ -94,6 +99,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
